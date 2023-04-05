e75dd64493e2aad6c53251eb6daeb5aaa57008bb
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-04-05 10:19:25 -0300

    aula 05.04.23

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿//Um programa que receba o nome do usuario e a senha
+//A senha deve ter 6 caracteres ex.:"admin"
+//validar a senha
+//enquanto a senha nao ativar regra pedida o programa nao encerra e solicite que seja digitado uma senha no padrao requerido
+//a senha 6 caracteres exiba mensagem ok
+
+//Console.WriteLine($"digite o seu nome:");
+//string nome = Console.ReadLine();
+//Console.WriteLine($"digite a sua senha qu (tem que ter 6 caracteres)");
+
+//string senha = Console.ReadLine();
+//while (senha.Length != 6);
+//{
+//   Console.WriteLine($"Digite uma senha no padrao requerido (com 6 caracteres):");
+//   senha = Console.ReadLine();
+//}
+//Console.WriteLine($"Usuario e senha recebidas com sucesso!");
+
+//Exercicio aula 05.04.2023
+//Faca um programa 
+
+//Nome diferente de vazio
+//idade de zero a 100 anos
+//estado civil
+
+
+Console.WriteLine($"Escreva seu nome:");
+String nome = Console.ReadLine();
+
+
+if (string.IsNullOrEmpty(nome));
+{
+    Console.WriteLine($"O nome nao pode estar vazio, repita ");
+}
+Console.WriteLine($"Declare seu estado civil conforme abaixo");
+Console.WriteLine($"c-casado");  
+Console.WriteLine($"s-solteiro");  
+Console.WriteLine($"d-divorciado");
+string estadoCivil = Console.ReadLine();
+
+Console.WriteLine($"Escreva sua idade");
+
+//($"Escreva sua idade "maior que zero" "menor que 100");
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
